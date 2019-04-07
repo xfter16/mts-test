a58f9343e8c3902f59fd311c659ab6abe3f5bf0e
Sergey Vagabov <svagabov@bianca-city.ru>
2019-04-07 19:03:49 +0300

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  login: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+
+  @Column()
+  deletedAt: Date;
+}
